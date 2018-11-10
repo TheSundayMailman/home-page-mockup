9e1e111b699c7601f2afa8f560b9c5283f39ca2e
Albert Sare <thesundaymailman@gmail.com>
2018-11-09 18:30:53 -0800

    'where-to' section layout roughly finished

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import '../styles/main.css';
+
 function Main(props) {
   return (
     <main>
-      <h1>Main here</h1>
+      <section className="where-to">
+        <div className="where-to-title-container">
+          <h1>Where to?</h1>
+        </div>
+      </section>
+      <section>
+        <h1>Beyond ride sharing</h1>
+      </section>
+      <section>
+        <h1>Safety</h1>
+      </section>
+      <section>
+        <h1>Latest news</h1>
+      </section>
+      <section>
+        <h1>Doors are always opening</h1>
+      </section>
+      <section>
+        <h1>About, News, Community</h1>
+      </section>
+      <section>
+        <h1>Signup</h1>
+      </section>
     </main>
   );
 }
